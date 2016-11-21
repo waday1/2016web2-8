4e386f502f9b21772b40cc82297fc84d0030284a
waday1 <g1527079@st.t-kougei.ac.jp>
2016-11-21 15:50:40 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+onload=function(){
+  var canvas=document.getElementById('canvas');
+  canvas.width=500;
+  canvas.height=300;
+  
+  var gl=canvas.getContext('webgl')||canvas.getContext('experimental-webgl');
+  
+  gl.clearColor(0.0, 0.0, 0.0, 1.0):
+  bg.clear(gl.COLOR_BUFFER_BIT);
+};
